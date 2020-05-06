92f6de54f866161b858932442c3e14a10df845b4
Dominik <dominik.behrend@web.de>
2020-05-06 16:21:31 +0200

    06.05

--- maize_paper.R
+++ maize_paper.R
@@ -2,12 +2,10 @@ library(tidyverse)
 library(readxl)
 library(gridExtra)
 library(ggpubr)
-library(plot3D)
 library(reshape2)
+library(lme4)
 
 maize <- read_excel("Maize dataset.xlsx")
-
-maize <- data.frame(maize, FM_ganze_Pflanze = as.numeric(maize$FM_ganze_Pflanze))
 
 #1. checking for normal distribution
 #1.1 without treatment differentiation
@@ -447,14 +445,67 @@ d <- ggscatter(kontr, x = "ChlNDI", y = "Chl_Labor",
 
 ggarrange(a, b, c, d)
 
+#phenological plots
+#height -> strange error if you add notches, maybe look at this again
+q <- ggplot(maize, aes(x = Treatment, y = Pflanzenhoehe, fill = Treatment)) +
+  geom_boxplot(show.legend = FALSE) +
+  ggtitle("A") +
+  ylab("Plant height [cm?]") +
+  geom_jitter(position = position_jitter(0.2), show.legend = FALSE) +
+  theme_bw() 
+q
 
+#leafe density
+w <- ggplot(maize, aes(x = Treatment, y = Blattdichte, fill = Treatment)) +
+  geom_boxplot(show.legend = FALSE) +
+  ggtitle("B") +
+  ylab("Leafe density [Leaves/?]") +
+  geom_jitter(position = position_jitter(0.2), show.legend = FALSE) +
+  theme_bw() 
+w
 
+ggarrange(q, w)
 
+#testing if the treatments have a sig. effect on the Plantheight
+h1 <- lm(Pflanzenhoehe ~ Treatment, data = maize)
+summary(h1)
 
+h0 <- lm(Pflanzenhoehe ~ 1, data = maize)
+summary(h0)
 
+par(mfrow = c(2, 2))
+plot(h1)
+plot(h0)
+par(mfrow = c(1, 1))
 
+shapiro.test(maize$Pflanzenhoehe)
 
+anova(h1, h0) #h1 is definitely the better model, but seems to be not nd
+#p-value if we use lm and expect this to be nd: 0.0003835 -> Treatments have an effect
 
+#using glm 
+h1 <- glm(Pflanzenhoehe ~ Treatment, data = maize)
+summary(h1)
+#Nährstoffmangel seems to have a significant effect, trockenstress not??? (not sure about this, 
+#never used a glm)
+
+#testing if the treatments have a sig. effect on the chl_lab content
+h1 <- lm(Chl_Labor ~ Treatment, data = maize)
+summary(h1)
+
+h0 <- lm(Chl_Labor ~ 1, data = maize)
+summary(h0)
+
+par(mfrow = c(2, 2))
+plot(h1)
+plot(h0)
+par(mfrow = c(1, 1))
+
+shapiro.test(maize$Chl_Labor)
+
+anova(h1, h0)
+#The treatments seem to have a sign effect on the chl lab content and nd seems at least more likely
+#p-value: 0.007401
 
 
 
